c62783f99e8e9df61b9db683a2ab8a659f2b0831
Kim Bergstrom <kimmen_b@hotmail.com>
2023-06-12 15:41:37 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [ #IAM
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('travel_Guide')
+
+region = SHEET.worksheet('region')
+
+data = region.get_all_values()
+print(data)
+
+
+
+# TRAVEL GUIDE - CURRENCY EXCHANGE
