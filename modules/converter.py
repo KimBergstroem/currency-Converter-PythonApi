913d853676320a885eb0bcad235da66969aa4789
Kim Bergstrom <kimmen_b@hotmail.com>
2023-06-13 00:58:37 +0000

    Add: try and except on currency inputs

--- modules/converter.py
+++ modules/converter.py
@@ -7,41 +7,43 @@ import requests
 
 
 def convert_currency():
-    """
-    Input form user along with questions about what currency to convert. The function will convert the amount
-    and display the exchange rate up to date with the API
-    """
-   init_currency = input('Enter an initial currency: ')
-   target_currency = input('Enter a target currency: ')
+    while True:
+        init_currency = input('Enter an initial currency: ')
+        target_currency = input('Enter a target currency: ')
+        
+        while True:
+            try:
+                amount = float(input('Enter the amount: '))
+            except:
+                print('The amount must be a numeric value!')
+                continue
 
-   while True:
-       try:
-           amount = float(input('Enter the amount: '))
-       except:
-           print('The amount must be a numeric value!')
-           continue
+            if not amount > 0:
+                print('The amount must be greater than 0')
+                continue
+            else:
+                break
 
-       if not amount > 0:
-           print('The amount must be greater than 0')
-           continue
-       else:
-           break
+        url = ('https://api.apilayer.com/fixer/convert?to='
+                + target_currency + '&from=' + init_currency +
+                '&amount=' + str(amount))
 
-   url = ('https://api.apilayer.com/fixer/convert?to='
-          + target_currency + '&from=' + init_currency +
-          '&amount=' + str(amount))
+        payload = {}
+        headers = {'apikey': 'Wyf6JKlmEnpBYW6kzAUSsoo0ptkUz5lr'}
+        response = requests.request('GET', url, headers=headers, data=payload)
+        status_code = response.status_code
 
-   payload = {}
-   headers = {'apikey': 'Wyf6JKlmEnpBYW6kzAUSsoo0ptkUz5lr'}
-   response = requests.request('GET', url, headers=headers, data=payload)
-   status_code = response.status_code
+        if status_code != 200:
+            print('Uh oh, there was a problem. Please try again later')
+            quit()
 
-   if status_code != 200:
-       print('Uh oh, there was a problem. Please try again later')
-       quit()
+        result = response.json()
+        print('Conversion result: ' + str(result['result']))
 
-   result = response.json()
-   print('Conversion result: ' + str(result['result']))
+        repeat = input(f"Do you wanna convert again? (y / n): \n")
+        if repeat != "y":
+            break
+
 
 if __name__ == '__main__':
    convert_currency()
